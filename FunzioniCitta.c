692869bf4b6a6d4d63cda4522b5f3aaee2f4afff
giantrix8 <96435444+giantrix8@users.noreply.github.com>
2022-06-01 23:34:48 +0200

    Modifiche su elimina nodo
    Sono state rese iterative alcune funzioni ricorsive perchè non ritornavano il nodo giusto ma tornavano la radice ed è cambiata la funzione che elimina una città. In più è stata aggiunta la funzione che trova il padre di un certo nodo.

--- FunzioniCitta.c
+++ FunzioniCitta.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include "strutture.h"
 
+
+
 static AlberoCitta *InserisciCitta(char *nome,short aereo,short treno) {
     AlberoCitta *tmp;
     tmp=(AlberoCitta*)malloc(sizeof(AlberoCitta));
@@ -17,7 +19,7 @@ static AlberoCitta *InserisciCitta(char *nome,short aereo,short treno) {
     strcpy(tmp->citta->nome,nome);
     tmp->citta->aereo=aereo;
     tmp->citta->treno=treno;
-    tmp->citta->ListaAereo;
+    //tmp->citta->ListaHotel=hotel;
     return tmp;
 }
 static AlberoCitta *RiempiAlberoCitta(AlberoCitta *radice, char *nome, short aereo, short treno){
@@ -36,23 +38,23 @@ static AlberoCitta *RiempiAlberoCitta(AlberoCitta *radice, char *nome, short aer
     }
     return radice;
 }
-
-static AlberoCitta *CercaNodo(AlberoCitta *radice,char *nome,int *errore) {
+static AlberoCitta *CercaNodo(AlberoCitta *radice,char *nome) {
     int controllo;
-    AlberoCitta *result = NULL;
-    
-    controllo = strcmp(radice->citta->nome, nome);
-    if (controllo == 0) {
-        *errore=1;
-        result = radice;
+    AlberoCitta *nodo;
+    nodo=radice;
+    while(nodo!=NULL){
+        controllo=strcmp(nodo->citta->nome, nome);
+        if (controllo==0){
+            return nodo;
+        }
+        else if (controllo==1){
+            nodo=nodo->sx;
+        }
+        else {
+            nodo=nodo->dx;
+        }
     }
-    else if (controllo > 0) {
-        result = CercaNodo(radice->sx, nome,errore);
-    }
-	else {
-        result = CercaNodo(radice->dx, nome,errore);
-    }
-    return result;
+    return NULL;
 }
 
 static ListaNext *InserisciDestinazione(ListaNext *testa,AlberoCitta *destinazione, float prezzo, int tempo){
@@ -69,14 +71,13 @@ static ListaNext *InserisciDestinazione(ListaNext *testa,AlberoCitta *destinazio
     return tmp;
 }
 
-//tipo==1 se si aggiunge nella lista aereo, tipo==0 se si aggiunge nella lista treno
+//tipo==1 se si aggiunge nella lista aereo
 static void InserisciListaAdiacenza(AlberoCitta *radice,char *partenza, char *destinazione, float prezzo, int tempo,short tipo){
     AlberoCitta *CittaPartenza,*CittaDestinazione;
-    int errore=0;
-    CittaPartenza=CercaNodo(radice,partenza,&errore);
-    if (errore==1){
-        CittaDestinazione=CercaNodo(radice,destinazione,&errore);
-        if (errore==1){
+    CittaPartenza=CercaNodo(radice,partenza);
+    if (CittaPartenza==NULL){
+        CittaDestinazione=CercaNodo(radice,destinazione);
+        if (CittaDestinazione==NULL){
             if (tipo==1) {
                 CittaPartenza->citta->ListaAereo = InserisciDestinazione(CittaPartenza->citta->ListaAereo,CittaDestinazione, prezzo, tempo);
             }
@@ -93,18 +94,32 @@ static void InserisciListaAdiacenza(AlberoCitta *radice,char *partenza, char *de
         printf("\nLa citt%c di partenza non %c stata trovata",a_accentata, e_accentata);
     }
 }
-static void CercaFogliaMinAlberoCitta(AlberoCitta **radice){
-    AlberoCitta *tmp;
-    tmp=*radice;
-    if (tmp->sx!=NULL){
-        CercaFogliaMinAlberoCitta(&tmp->sx);
+static *CercaPadre(AlberoCitta *radice, char *nome){
+    AlberoCitta *padre=NULL,*tmp=radice;
+    int trovato=0,controllo;
+    while(tmp!=NULL && trovato==0){
+        controllo= strcmp(radice->citta->nome,nome);
+        if (controllo==0){
+            trovato=1;
+        }
+        else if (controllo>0){
+            padre=tmp;
+            tmp=tmp->sx;
+        }
+        else{
+            padre=tmp;
+            tmp=tmp->dx;
+        }
     }
-    else if (tmp->sx==NULL && tmp->dx!=NULL){
-        CercaFogliaMinAlberoCitta(&tmp->dx);
+return padre;
+}
+
+static AlberoCitta *CercaFogliaMinAlberoCitta(AlberoCitta *radice){
+    AlberoCitta *tmp=radice;
+    while(tmp->sx!=NULL){
+        tmp=tmp->sx;
     }
-    else if (tmp->sx==NULL && tmp->dx==NULL){
-        *radice=tmp;
-    }
+    return tmp;
 }
 static void DeallocaListaCitta(ListaNext *lista, char *nome){
     if (lista!=NULL){
@@ -128,149 +143,107 @@ static void EliminaListaCitta(AlberoCitta *radice, char*nome){
         EliminaListaCitta(radice->dx,nome);
     }
 }
-static void FreeListaCitta(AlberoCitta *radice){
+static void FreeListeAdiacenzaCitta(AlberoCitta *radice){
+    ListaNext *tmp;
     while (radice->citta->ListaAereo!=NULL) {
+        tmp=radice->citta->ListaAereo->next;
         free(radice->citta->ListaAereo);
+        radice=tmp;
     }
     while (radice->citta->ListaTreno!=NULL) {
+        tmp=radice->citta->ListaTreno->next;
         free(radice->citta->ListaTreno);
-    }
+        radice=tmp;
+    }/*
     while (radice->citta->ListaHotel!=NULL) {
+        tmp=radice->citta->ListaTreno->next;
         free(radice->citta->ListaHotel);
-    }
-    free(radice->citta);
+        radice=tmp;
+    }*/
 }
 
-AlberoCitta *EliminaNodo(AlberoCitta *padre,AlberoCitta *radice,char *nome){
-    if(radice!=NULL) {
-        if (strcmp(radice->citta->nome,nome)==0){
-            FreeListaCitta(radice);
-            if (radice->sx==NULL && radice->dx==NULL){
+static AlberoCitta *EliminaNodoCitta(AlberoCitta *TestaAlbero,char *nome){
+    if(TestaAlbero!=NULL) {
+        AlberoCitta *radice=TestaAlbero;
+        AlberoCitta *padre;
+        radice= CercaNodo(radice,nome);
+        if (radice==NULL){
+            printf("/nIl nodo da eliminare non %c presente.",e_accentata);
+            return TestaAlbero;
+        }
+        FreeListeAdiacenzaCitta(radice);
+        free(radice->citta);
+        if (radice->sx==NULL && radice->dx==NULL){
+            free(radice);
+            radice=NULL;
+        }
+        else if(radice->sx!=NULL && radice->dx!=NULL){
+            AlberoCitta *Min;
+            Min=CercaFogliaMinAlberoCitta(radice->dx); //in Min c'è il nodo minimo del sottoalbero destro del nodo da eliminare
+            padre=CercaPadre(TestaAlbero,Min->citta->nome);//in padre c'è il padre di min
+            if (padre==NULL && radice->dx->sx==NULL) {//Da eliminare è la radice e l'elemento più piccolo del sottoalbero dx è il figlio stesso
+                Min->sx=TestaAlbero->sx;
+                TestaAlbero=Min;
                 free(radice);
             }
-            else if(radice->sx!=NULL && radice->dx!=NULL){
-                AlberoCitta *tmp=radice;
-                CercaFogliaMinAlberoCitta(&tmp->dx);
-                radice->citta=tmp->citta;
-                free(tmp);
-            }
-            else if (radice->sx!=NULL && radice->dx==NULL){
-                padre->sx=radice->sx;
-                free(radice);
-                radice=padre->sx;
-            }
-            else if (radice->sx==NULL && radice->dx!=NULL){
-                padre->dx=radice->dx;
-                free(radice);
-                radice=padre->dx;
+            else
+            {
+                padre->sx = Min->dx;
+                radice->citta = Min->citta;
+                free(Min);
             }
         }
-        else {
-            radice = EliminaNodo(radice, radice->sx, nome);
-            radice = EliminaNodo(radice, radice->dx, nome);
+        else if (radice->sx!=NULL && radice->dx==NULL){
+            padre->sx=radice->sx;
+            free(radice);
+            radice=padre->sx;
+        }
+        else if (radice->sx==NULL && radice->dx!=NULL){
+            padre->dx=radice->dx;
+            free(radice);
+            radice=padre->dx;
         }
     }
-    return radice;
+    return TestaAlbero;
 }
 AlberoCitta *EliminaCitta(AlberoCitta *testa,char *nome){
-    testa=EliminaNodo(NULL,testa,nome);
+    testa=EliminaNodoCitta(testa,nome);
     EliminaListaCitta(testa,nome);
     return testa;
 }
-
 AlberoCitta *carica_grafo(AlberoCitta *radice)
- {
- 	FILE *fp = NULL;
- 	AlberoCitta *currentNodo;
- 	Citta *currentCitta;
- 	char currentName[LenC], destName[LenC];
- 	short treno, aereo;
- 	float prezzo;
- 	int durata, errore = 0;
- 	
- 	//Caricamento albero delle città
- 	fp = fopen("citta.txt", "r");
- 	while(fscanf(fp, "%s", currentName) > 0)
- 	{
- 		fscanf(fp, "%d", &aereo);
- 		fscanf(fp, "%d", &treno);	
- 		radice = RiempiAlberoCitta(radice, currentName, aereo, treno);	
-	}
- 	fclose(fp);
- 	
- 	//Caricamento liste di adiacenza
- 	fp = fopen("adiacenze.txt", "r");
- 	while(fscanf(fp, "%s", currentName) > 0)
- 	{
- 		//Inserimenti in lista treni
- 		while(fscanf(fp, "%s %f %d", destName, &prezzo, &durata) > 0)
- 		{
- 			if(strcmp("0", destName) == 0)
-			 	break;
-			InserisciListaAdiacenza(radice, currentName, destName, prezzo, durata, 0);;
-		}
-		//inserimenro in lista aerei
- 		while(fscanf(fp, "%s %f %d", destName, &prezzo, &durata) > 0)
- 		{
- 			if(strcmp("0", destName) == 0)
-			 	break;
-			InserisciListaAdiacenza(radice, currentName, destName, prezzo, durata, 1);
-		}
-	}
-	fclose(fp);
-	return radice;
-}
-
-void salva_citta(AlberoCitta *radice, FILE *fp)
 {
-	if(radice != NULL)
-	{
-		fprintf(fp, "%s ", radice->citta->nome);
-		fprintf(fp, "%d %d\n", radice->citta->treno, radice->citta->aereo);
-		salva_citta(radice->sx, fp);
-		salva_citta(radice->dx, fp);
-	}
-}
+    FILE *fp = NULL;
+    char currentName[LenC], destName[LenC];
+    short treno, aereo;
+    float prezzo;
+    int durata;
 
-void salva_adiacenze(AlberoCitta *radice, FILE *fp)
-{
-	ListaNext *curr;
-	if(radice != NULL)
-	{
-		fprintf(fp, "%s ", radice->citta->nome);
-		//Salva lista treni
-		curr = radice->citta->ListaTreno;
-		while (curr != NULL)
-		{
-			fprintf(fp, "%s ", curr->citta->nome);
-			fprintf(fp, "%f ", curr->prezzo);
-			fprintf(fp, "%d ", curr->durata);
-			curr = curr->next;
-		}
-		fprintf(fp, "0 ");
-		//Salva lista aerei
-		curr = radice->citta->ListaAereo;
-		while (curr != NULL)
-		{
-			fprintf(fp, "%s ", curr->citta->nome);
-			fprintf(fp, "%f ", curr->prezzo);
-			fprintf(fp, "%d ", curr->durata);
-			curr = curr->next;
-		}
-		fprintf(fp, "0\n");
-		salva_adiacenze(radice->sx, fp);
-		salva_adiacenze(radice->dx, fp);
-	}
-}
+    //Caricamento albero delle città
+    fp = fopen("citta.txt", "r");
+    while(fscanf(fp, "%s %d %d", currentName, &treno, &aereo) > 0)
+        radice = RiempiAlberoCitta(radice, currentName, aereo, treno);
+    fclose(fp);
 
-void salva_grafo(AlberoCitta *radice)
- {
- 	FILE *fp;
- 	fp = fopen("citta.txt", "w");
- 	salva_citta(radice, fp);
- 	fclose(fp);
- 	
- 	fp = fopen("adiacenze.txt", "w");
- 	salva_adiacenze(radice, fp);
- 	fclose(fp);
- }
+    //Caricamento liste di adiacenza
+    fp = fopen("adiacenze.txt", "r");
+    while(fscanf(fp, "%s", currentName) > 0)
+    {
+        //Inserimenti in lista treni
+        while(fscanf(fp, "%s %f %d", destName, &prezzo, &durata) > 0)
+        {
+            if(strcmp("0", destName) == 0)
+                break;
+            InserisciListaAdiacenza(radice, currentName, destName, prezzo, durata, 0);
+        }
+        //inserimenro in lista aerei
+        while(fscanf(fp, "%s %f %d", destName, &prezzo, &durata) > 0)
+        {
+            if(strcmp("0", destName) == 0)
+                break;
+            InserisciListaAdiacenza(radice, currentName, destName, prezzo, durata, 1);
+        }
+    }
+    fclose(fp);
+    return radice;
+}
