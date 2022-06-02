5a3777e7670aa1f97f5fefedd85325864824506e
Mandr96 <98476394+Mandr96@users.noreply.github.com>
2022-06-02 21:52:49 +0100

    Update DijkstraAlgo.c

--- DijkstraAlgo.c
+++ DijkstraAlgo.c
@@ -53,9 +53,9 @@ void Heapfy(DjkElem **minHeap, int i, int dim, int modo)
     int min = i;
     int L = 2*i+1;
     int R = 2*i+2;
-    if(L < dim && (minHeap[L]->peso[modo] < minHeap[min]->peso[modo] || minHeap[L]->peso[modo] == -1))
+    if(L < dim && (minHeap[min]->peso[modo] == -1 || (minHeap[L]->peso[modo] >= 0 && minHeap[L]->peso[modo] < minHeap[min]->peso[modo])))
         min = L;
-    if(R < dim && (minHeap[R]->peso[modo] < minHeap[min]->peso[modo] || minHeap[R]->peso[modo] == -1))
+    if(R < dim && (minHeap[min]->peso[modo] == -1 || (minHeap[R]->peso[modo] >= 0 && minHeap[R]->peso[modo] < minHeap[min]->peso[modo])))
         min = R;
     if(min != i)
     {
@@ -71,11 +71,11 @@ void BuildHeap(DjkElem **minHeap, int dim, int modo)
         Heapfy(minHeap, i, dim, modo);
 }
 
-Citta *EstraiMinimo(DjkElem **minHeap, int *dim)
+Citta *EstraiMinimo(DjkElem **minHeap, int dim)
 {
     Citta *minCitta = minHeap[0]->citta;
-    DjkSwap(minHeap, 0, *dim-1);
-    *dim--;
+    printf("nodo piu vicino: %s\n", minCitta->nome);
+    DjkSwap(minHeap, 0, dim-1);
     return minCitta;
 }
 
@@ -84,10 +84,13 @@ DjkElem **djkRelax(DjkElem **Vector, Citta *partenza, ListaNext *arco, int modo)
     float newPeso;
     Citta *dest = arco->citta;
 
+
     if(modo == 0)
         newPeso = Vector[partenza->key]->peso[modo] + arco->durata;
     else
         newPeso = Vector[partenza->key]->peso[modo] + arco->prezzo;
+
+    printf("%s %f + per %s %f = %f\n", partenza->nome, Vector[partenza->key]->peso[modo], dest->nome, arco->durata, newPeso);
 
     if(Vector[dest->key]->peso[modo] == -1 || newPeso < Vector[dest->key]->peso[modo]){
         Vector[dest->key]->peso[modo] = newPeso;
@@ -96,12 +99,19 @@ DjkElem **djkRelax(DjkElem **Vector, Citta *partenza, ListaNext *arco, int modo)
     return Vector;
 }
 
+void stampaPesi(DjkElem **vett, int dim)
+{
+    int i;
+    for(i = 0; i < dim; i++)
+        printf("per %s %f minuti\n", vett[i]->citta->nome, vett[i]->peso[0]);
+}
+
 DjkElem **MinPath(Citta *partenza, DjkElem **Vector, DjkElem **minHeap, int dim, int tipo, int modo)
 {
     if(dim != 0)
     {
         Citta *minimo = NULL;
-        ListaNext  *curr;
+        ListaNext  *curr = NULL;
         if(tipo == 0)
             curr = partenza->ListaAereo;
         else
@@ -112,17 +122,18 @@ DjkElem **MinPath(Citta *partenza, DjkElem **Vector, DjkElem **minHeap, int dim,
             curr = curr->next;
         }
         BuildHeap(minHeap, dim, modo);
-        minimo = EstraiMinimo(minHeap, &dim);
+        minimo = EstraiMinimo(minHeap, dim--);
         minHeap = MinPath(minimo, Vector, minHeap, dim, tipo, modo);
         return minHeap;
     }
 }
 
-void stampaPesi(DjkElem **vett, int dim)
+Path *inserisciPath(Path *top, Citta *citta)
 {
-    int i;
-    for(i = 0; i < dim; i++)
-        printf("Città: %s %d minuti\n", vett[i]->citta->nome, vett[i]->peso[0]);
+    Path *nuovo = (Path*)malloc(sizeof(Path));
+    nuovo->citta = citta;
+    nuovo->next = top;
+    return nuovo;
 }
 
 // tipo: 0 per l'aereo, 1 per il treno
@@ -130,17 +141,42 @@ void stampaPesi(DjkElem **vett, int dim)
 Path *FindMinPath(AlberoCitta *radice, Citta *partenza, Citta *arrivo, int tipo, int modo)
 {
     int dim = contaCitta(radice);
+    Path *minPath = NULL;
     DjkElem **Vector = NULL;
     DjkElem **minHeap = NULL;
+    DjkElem *currElem = NULL;
+    Citta *currCitta = NULL;
 
     Vector = newVector(Vector, dim);
     minHeap = newMinHeap(minHeap, dim);
     riempiDjk(radice, Vector, minHeap);
 
-    printf("%s", Vector[partenza->key]->citta->nome);
     Vector[partenza->key]->peso[modo] = 0;
-    BuildHeap(minHeap, dim--, modo);
+    BuildHeap(minHeap, dim, modo);
+    partenza = EstraiMinimo(minHeap, dim--);
     minHeap = MinPath(partenza, Vector, minHeap, dim, tipo, modo);
+
+    printf("\n");
     stampaPesi(Vector, dim);
-    return NULL;
+
+    currCitta = arrivo;
+    currElem = Vector[arrivo->key];
+    while(1)
+    {
+        minPath = inserisciPath(minPath, currCitta);
+        currCitta = currElem->prev;
+        if(currCitta == NULL)
+            break;
+        currElem = Vector[currCitta->key];
+    }
+    return minPath;
+}
+
+void stampa_path(Path *cammino)
+{
+    if(cammino != NULL)
+    {
+        printf(" --> %s", cammino->citta->nome);
+        stampa_path(cammino->next);
+    }
 }
