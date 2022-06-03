5149dc1e50d5fc7c01fedf2d1a730ccedea7121e
giantrix8 <96435444+giantrix8@users.noreply.github.com>
2022-06-03 16:20:17 +0200

    Add files via upload

--- Hotel.c
+++ Hotel.c
@@ -3,29 +3,6 @@
 #include <string.h>
 #include "strutture.h"
 
-static void OrdinaVettoreHotel(GrafoHotel *citta){
-    int fine=citta->dim, i;
-    int scambio, indice;
-    Hotel *tmp;
-    char max[LenC];
-    strcpy(max,citta->hotel[0]->nome);
-    while (fine>0){
-        scambio==0;
-        for(i=0;i<fine;i++){
-            if (strcmp(max,citta->hotel[i]->nome)<0){
-                indice=i;
-                scambio==1;
-            }
-        }
-        if (scambio==1){
-            tmp=citta->hotel[indice];
-            citta->hotel[indice]=citta->hotel[fine];
-            citta->hotel[fine]=tmp;
-        }
-        fine--;
-    }
-}
-
 static GrafoHotel *CaricaVetHotel(FILE **file,int dim){
     GrafoHotel *albergo=NULL;
     albergo = (GrafoHotel *) malloc(sizeof(GrafoHotel));
@@ -46,7 +23,6 @@ static GrafoHotel *CaricaVetHotel(FILE **file,int dim){
             strcpy(albergo->hotel[i]->nome, citta);
             printf("->%s %.2f",albergo->hotel[i]->nome,albergo->hotel[i]->prezzo);
         }
-        //OrdinaVettoreHotel(albergo);
     }
     return albergo;
 }
